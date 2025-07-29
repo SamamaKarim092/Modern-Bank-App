b9ea0e181cf18e6626430e21ed1ec631ddac3b5a
SamamaKarim092 <samamakarim90@gmail.com>
2025-07-29 20:24:36 +0500

    navbar with the rounded raindbow hover effect

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,20 +11,29 @@ const Navbar = () => {
     <nav className="w-full flex py-6 justify-between items-center navbar">
       <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
 
-      <ul className="list-none sm:flex hidden justify-end items-center flex-1 text-white">
-        {navLinks.map((nav, index) => (
-          <li
-            key={nav.id}
-            className={`font-poppins font-normal cursor-pointer text-[16px] ${
-              active === nav.title ? "text-white" : "text-dimWhite"
-            } ${index === navLinks.length - 1 ? "mr-0" : "mr-10"}`}
-            onClick={() => setActive(nav.title)}
-          >
-            <a href={`#${nav.id}`}>{nav.title}</a>
-          </li>
-        ))}
-      </ul>
+      {/* Desktop Navigation */}
+      <div className="sm:flex hidden flex-1 justify-end items-center">
+        <ul className="list-none flex justify-end items-center text-white">
+          {navLinks.map((nav, index) => (
+            <li
+              key={nav.id}
+              className={`font-poppins font-normal cursor-pointer text-[16px] ${
+                active === nav.title ? "text-white" : "text-dimWhite"
+              } ${index === navLinks.length - 1 ? "mr-0" : "mr-10"}`}
+              onClick={() => setActive(nav.title)}
+            >
+              <a 
+                href={`#${nav.id}`}
+                className="rainbow-hover px-4 py-2 rounded-lg transition-all duration-200 inline-block"
+              >
+                {nav.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
 
+      {/* Mobile Navigation */}
       <div className="sm:hidden flex flex-1 justify-end items-center">
         <img
           src={toggle ? close : menu}
@@ -38,7 +47,7 @@ const Navbar = () => {
             !toggle ? "hidden" : "flex"
           } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
         >
-          <ul className="list-none flex justify-end items-start flex-1 flex-col  text-white">
+          <ul className="list-none flex justify-end items-start flex-1 flex-col text-white">
             {navLinks.map((nav, index) => (
               <li
                 key={nav.id}
@@ -47,7 +56,12 @@ const Navbar = () => {
                 } ${index === navLinks.length - 1 ? "mb-0" : "mb-4"}`}
                 onClick={() => setActive(nav.title)}
               >
-                <a href={`#${nav.id}`}>{nav.title}</a>
+                <a 
+                  href={`#${nav.id}`}
+                  className="rainbow-hover px-4 py-2 rounded-lg block transition-all duration-200"
+                >
+                  {nav.title}
+                </a>
               </li>
             ))}
           </ul>
